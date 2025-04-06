bb66f6266cfa688ae466dad77879a9ae2aaebe38
aayushaghera <132937937+aayushaghera@users.noreply.github.com>
2025-04-06 16:43:01 +0530

    setup Express app with middleware, DB connection, and routing

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,23 @@
-const express = require("express");
+// backend/index.js
+const express = require('express');
+const cors = require('cors');
 
+const rootRouter = require('./routes/index'); // Importing the routes from routes/index.js
+
+const app = express(); // Initialize Express app
+
+// Middleware
+app.use(cors()); // Enable Cross-Origin Resource Sharing
+app.use(express.json()); // Parse JSON request bodies
+
+const { connectDB } = require('./db');
+const { connect } = require('mongoose');
+connectDB();
+
+// API Routes
+app.use("/api/v1", rootRouter); // Use rootRouter for any routes prefixed with /api/v1
+
+// Start the Server
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
